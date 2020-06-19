975bfaa702103405d1ac52198166e6b923da5eb4
ScottBot10 <scott.faurholm@gmail.com>
2020-06-19 22:33:16 +0200

    Added config command

--- scott_bot/cogs/config_cog.py
+++ scott_bot/cogs/config_cog.py
@@ -1,1 +1,21 @@
-print("Hello from: " + __file__)
+from discord.ext import commands
+from discord.ext.commands import Context
+
+from scott_bot.bot import Bot
+
+
+class Config(commands.Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @commands.group(name='config', aliases=('cfg',), invoke_without_command=True)
+    async def config_group(self, ctx: Context):
+        print("config")
+
+    @config_group.command(name='help')
+    async def config_help(self, ctx: Context):
+        print("help")
+
+
+def setup(bot: Bot):
+    bot.add_cog(Config(bot))
