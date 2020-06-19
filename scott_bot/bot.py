5b7bf7b264b08281886b0bc98a74c40f46670c4e
ScottBot10 <scott.faurholm@gmail.com>
2020-06-19 22:12:01 +0200

    Enabled database functionality

--- scott_bot/bot.py
+++ scott_bot/bot.py
@@ -9,7 +9,7 @@ class Bot(commands.Bot):
     def __init__(self, command_prefix, **options):
         super().__init__(command_prefix, **options)
         self.http_session: Optional[aiohttp.ClientSession] = None
-        # self.db_conn: Optional[asyncpg.Connection] = None
+        self.db_conn: Optional[asyncpg.Connection] = None
 
     async def login(self, token, *, bot=True):
         await self._recreate()
@@ -21,14 +21,15 @@ class Bot(commands.Bot):
 
     async def _create(self) -> None:
         self.http_session = aiohttp.ClientSession()
-        # self.db_conn = await asyncpg.connect(self.conf.get('db_link'))
+        self.db_conn = await asyncpg.connect(self.conf.get('db_link'))
 
     async def _close(self) -> None:
+        print("closing")
         if self.http_session:
             await self.http_session.close()
 
-        # if self.db_conn:
-        #     await self.db_conn.close()
+        if self.db_conn:
+            await self.db_conn.close()
 
     async def _recreate(self) -> None:
         await self._close()
