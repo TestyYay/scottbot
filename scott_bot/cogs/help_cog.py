3987e4866afab30a511804e2f1a5b08fb6c4f5d9
ScottBot10 <scott.faurholm@gmail.com>
2020-06-20 17:45:47 +0200

    Fixed the help menu

--- scott_bot/cogs/help_cog.py
+++ scott_bot/cogs/help_cog.py
@@ -52,6 +52,7 @@ class Help(Cog):
                 if not getattr(cog, "hidden", False)
                 and len(cog.get_commands()) > 0
             ]
+            print("Hello")
             await HelpPaginator.paginate(
                 ctx,
                 embed,
