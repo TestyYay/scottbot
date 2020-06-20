e6853dfe5976d31be24184ffd1b0ecff8c9a33ce
ScottBot10 <scott.faurholm@gmail.com>
2020-06-20 23:27:58 +0200

    Made read-only config options

--- scott_bot/util/messages.py
+++ scott_bot/util/messages.py
@@ -46,7 +46,6 @@ async def wait_for_deletion(
 
 
 async def bad_arg_error(cog: Optional[Cog], ctx: Context, error: DiscordException):
-    print("bad_arg")
     if isinstance(error, BadArgument):
         await ctx.send(str(error.args[0]))
 
