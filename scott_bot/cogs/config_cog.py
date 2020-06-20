481cbc98cc0762d364d835ecaa3794ec0828a444
ScottBot10 <scott.faurholm@gmail.com>
2020-06-20 22:24:00 +0200

    Made read-only config options

--- scott_bot/cogs/config_cog.py
+++ scott_bot/cogs/config_cog.py
@@ -56,6 +56,7 @@ class Config(commands.Cog):
     @_config_group.error
     async def _config_group_error(self, ctx: Context, error):
         print("errored")
+        print(error)
         if isinstance(error, AttributeError):
             await ctx.send("You can't change that option.")
         else:
