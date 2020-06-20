10bceaf9db3fbde68b65b9c6162f3855e788cc51
ScottBot10 <scott.faurholm@gmail.com>
2020-06-20 21:27:39 +0200

    Added dad bot

--- scott_bot/util/config.py
+++ scott_bot/util/config.py
@@ -68,7 +68,8 @@ class _Config:
 async def get_config(name: str, bot, guild: Optional[discord.Guild] = None) -> Optional[_Config]:
     if bot.db_conn is not None:
         cols = await bot.db_conn.fetch(
-            f"SELECT column_name FROM information_schema.columns WHERE table_name = {DataBase.main_tablename};",
+            "SELECT column_name FROM information_schema.columns WHERE table_name = $1;",
+            DataBase.main_tablename
         )
         columns = [column.get("column_name") for column in cols]
         if name in columns:
