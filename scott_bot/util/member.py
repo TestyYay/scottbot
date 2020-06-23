e5d0916212848919cffa4c84fdefaf89508005e3
ScottBot10 <scott.faurholm@gmail.com>
2020-06-23 22:45:39 +0200

    Added resetnicks command

--- scott_bot/util/member.py
+++ scott_bot/util/member.py
@@ -30,7 +30,7 @@ async def kicplayer(ctx: Context, person):
 
 
 INSERT_SQL = """
-INSERT INTO {tablename} (guild_id, user_id, nick)
+INSERT INTO {tablename} (guild_id, user_id, nick, date)
     VALUES {vals};
 """
 
