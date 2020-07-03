5fb40334f9074dba3e987705aeea5764df0415b3
ScottBot10 <scott.faurholm@gmail.com>
2020-07-03 19:49:52 +0200

    Added with typing()

--- scott_bot/cogs/internet_cog.py
+++ scott_bot/cogs/internet_cog.py
@@ -27,12 +27,13 @@ class InternetCog(commands.Cog, name="Internet"):
         search_term = ' '.join(search_term)
         if self.bot.http_session is not None:
             query_string = urlencode({"search_query": search_term})
-            async with self.bot.http_session.get("http://www.youtube.com/results?" + query_string) as r:
-                if r.status == 200:
-                    text = await r.text()
-                    search_results = re.findall(r'href=\"/watch\?v=(.{11})', text)
-                    vid_id = random.choice(search_results)
-                    await ctx.send("http://www.youtube.com/watch?v=" + vid_id)
+            async with ctx.channel.typing():
+                async with self.bot.http_session.get("http://www.youtube.com/results?" + query_string) as r:
+                    if r.status == 200:
+                        text = await r.text()
+                        search_results = re.findall(r'href=\"/watch\?v=(.{11})', text)
+                        vid_id = random.choice(search_results)
+                        await ctx.send("http://www.youtube.com/watch?v=" + vid_id)
 
     @_yt_search.error
     async def yt_error(self, ctx: commands.Context, error):
@@ -54,15 +55,31 @@ class InternetCog(commands.Cog, name="Internet"):
         }
         term = ' '.join(term)
         if self.bot.http_session is not None:
-            async with self.bot.http_session.get("https://mashape-community-urban-dictionary.p.rapidapi.com/define",
-                                                 headers=headers, params={"term": term}) as r:
-                if r.status == 200:
-                    js = await r.json()
-                    definition = random.choice(js["list"]) if term.lower() != "scott" else js["list"][4]
-                    text = f'```"{definition["word"]}" Definition:\n\n\n{definition["definition"]}\n\n\nExample:\n\n\n{definition["example"]}\n\n\nAuthor: {definition["author"]}\nLikes: {definition["thumbs_up"]}``` '
-                    text = text.replace("]", "").replace("[", "")
-                    message = await ctx.send(text)
-                    await wait_for_deletion(message, (ctx.author,), client=self.bot)
+            message = None
+            async with ctx.channel.typing():
+                async with self.bot.http_session.get("https://mashape-community-urban-dictionary.p.rapidapi.com/define",
+                                                     headers=headers, params={"term": term}) as r:
+                    if r.status == 200:
+                        js = await r.json()
+                        definition = random.choice(js["list"]) if term.lower() != "scott" else js["list"][4]
+                        text = f'''```"{definition["word"]}" Definition:
+
+
+{definition["definition"]}
+
+
+Example:
+
+
+{definition["example"]}
+
+
+Author: {definition["author"]}
+Likes: {definition["thumbs_up"]}```'''
+                        text = text.replace("]", "").replace("[", "")
+                        message = await ctx.send(text)
+            if message is not None:
+                await wait_for_deletion(message, (ctx.author,), client=self.bot)
 
 
 def setup(bot: ScottBot):
